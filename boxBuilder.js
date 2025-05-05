4881c9eac5c8e1f65162b6f758c2f6ce91876dfe
Yellowguy08 <131368912+Yellowguy08@users.noreply.github.com>
2025-05-05 14:18:56 -0400

    Project page+ project box images

--- boxBuilder.js
+++ boxBuilder.js
@@ -1,20 +1,3 @@
-
-// let projects = ["Pin Park", "Fords Wellness", "Team Game", "Perhe", "Defog", "Yellowgames", "Scratch", "Gargoyle", "Centrellas Website", "Wind Data App"]
-let project = [
-
-				project("Park Pin","Pin where you park", "supercell.png"),
-				project("Ford Wellness", "School wellness block picker with live room updates.", "supercell.png"),
-				project("Team Game", "SurvivalIO Clone Team game project", "supercell.png"),
-				project("Perhe", "Friends Card Game Mobile port.", "supercell.png"),
-				project("Defog", "Vape detection app for school bathrooms.", "supercell.png"),
-				project("Yellowgames", "Archive of games I have published under Yellowgames, with the website that I build to display them.", "supercell.png"),
-				project("Scratch", "My scratch account with some of the project I have worked on.", "supercell.png"),
-				project("Centrellas Website", "My first large website project remaking a resturaunt website.", "supercell.png"),
-				project("Gargoyle", "A custom google browser face for ease of access to my classes when I was in High School.", "supercell.png"),
-				project("Wind Data App", "An app that tracks wind and weather data and displays the loaction and decodes the value.", "supercell.png")
-
-]
-
 class project {
 
 	constructor (name, desc, imgPth) {
@@ -23,32 +6,73 @@ class project {
 		this.imgPth = imgPth;
 	}
 
+	getName() {
+		return this.name;
+	}
+
+	getDesc() {
+		return this.desc;
+	}
+
+	getImgPth() {
+		return this.imgPth;
+	}
+
 }
 
+// let projects = ["Pin Park", "Fords Wellness", "Team Game", "Perhe", "Defog", "Yellowgames", "Scratch", "Gargoyle", "Centrellas Website", "Wind Data App"]
+let projects = [
 
-function projectPageBuilder(index) {
+	new project("Park Pin","Pin where you park", "images/pinPark.png"),
+	new project("Ford Wellness", "School wellness block picker with live room updates.", "images/fordsWellnessProfileView.png"),
+	new project("Team Game", "SurvivalIO Clone Team game project", "images/teamGame.png"),
+	new project("Perhe", "Friends Card Game Mobile port.", "images/perheHand.png"),
+	new project("Defog", "Vape detection app for school bathrooms.", "images/defogMap.png"),
+	new project("Yellowgames", "Archive of games I have published under Yellowgames, with the website that I build to display them.", "images/yellowGamesAbout.png"),
+	new project("Scratch", "My scratch account with some of the project I have worked on.", "images/scratchProfile.png"),
+	new project("Centrellas Website", "My first large website project remaking a resturaunt website.", "images/supercell.png"),
+	new project("Gargoyle", "A custom google browser face for ease of access to my classes when I was in High School.", "images/supercell.png"),
+	new project("Wind Data App", "An app that tracks wind and weather data and displays the loaction and decodes the value.", "images/windDataApp.png")
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+];
 
+function projectPageBuilder() {
+
+	// <div class="title">Title</span>
+	// <div class="content-container">
+	// 		<div class="desc">Desc</div>
+	// 		<img src="imgName.png">
+	// </div>
+
+	const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    indx = urlParams.get('project-id');
+
+	title = document.getElementById("title");
+	title.textContent = projects[indx].getName();
+
+	body = document.getElementById("project-body");
+
+	project_title = document.createElement("div");
+	project_title.classList.add("title");
+	project_title.textContent = projects[indx].getName();
+
+	content_container = document.createElement("div");
+	content_container.classList.add("content-container");
+
+	desc = document.createElement("div");
+	desc.classList.add("desc");
+	desc.textContent = projects[indx].getDesc();
+
+	img = document.createElement("img");
+	img.setAttribute("src", projects[indx].getImgPth());
+
+	content_container.appendChild(desc);
+	content_container.appendChild(img);
+
+	body.appendChild(project_title);
+	body.appendChild(content_container);
 
 }
 
@@ -61,14 +85,18 @@ function boxBuilder(index) {
 
 // 		HTML STRUCTURE
 //-------------------------------------------------------------------------
-// 		<div class="project-box">
-//  	   	<div class="project-title-box">
-//      	   <span class="project-title">Pin Park</span>
-//     		</div>
-// 		</div>
+// 		<a href="">
+//			<div class="project-box">
+//  	   		<div class="project-title-box">
+//      	   		<span class="project-title">Pin Park</span>
+//     			</div>
+// 			</div>
+// 		</a>
 
 	let grid = document.getElementById("Grid");
 
+	let link = document.createElement("a");
+	link.setAttribute("href", ("project_page.html?project-id=" + index));
 
 	let project_box = document.createElement("div");
 	let project_title_box = document.createElement("div");
@@ -81,12 +109,14 @@ function boxBuilder(index) {
 	project_title.classList.add("project-title");
 	project_title_box.classList.add("project-title-box");
 	project_box.classList.add("project-box");
+	project_box.setAttribute("style", "background-image: url(" + projects[index].getImgPth() + ");");
 
 
 	project_title_box.appendChild(project_title);
 	project_box.appendChild(project_title_box);
 
+	link.appendChild(project_box);
 
-	grid.appendChild(project_box);
+	grid.appendChild(link);
 
 }
