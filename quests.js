c189af99a6cc2c71b812328100d4ff90f2e86138
Yellowguy08 <131368912+Yellowguy08@users.noreply.github.com>
2025-06-02 14:15:59 -0400

    simplifying quest code

--- quests.js
+++ quests.js
@@ -1,115 +1,110 @@
-document.body.onload = questListBuilder();
 
-function questListBuilder() {
+// <div class="quest">
+//     <div class="quest-border">
+//         <span>Title</span>
+//         <span>Tag1 Tag2 Tag3 Tag4</span>
+//         <span class="quest-status">Completed</span>
+//     </div>
+// </div>
 
-    // <div class="quest">
-    //     <div class="quest-border">
-    //         <span>Title</span>
-    //         <span>Tag1 Tag2 Tag3 Tag4</span>
-    //         <span class="quest-status">Completed</span>
-    //     </div>
-    // </div>
+let quest_list = document.querySelector("#quest-list");
 
-    let quest_list = document.querySelector("#quest-list");
+if (quest_list) {
 
-    if (quest_list) {
+    projects.forEach(obj => {
+        let quest = document.createElement("div");
+        quest.classList.add("quest");
 
-        projects.forEach(obj => {
-            let quest = document.createElement("div");
-            quest.classList.add("quest");
+        quest.addEventListener("click", function() {
+            let quest_view = document.querySelector("#quest-view");
+            if (quest_view) {
 
-            quest.addEventListener("click", function() {
-                let quest_view = document.querySelector("#quest-view");
-                if (quest_view) {
+                quest_view.textContent = '';
+                // quest_view.removeChild(quest_view.lastChild);
 
-                    quest_view.textContent = '';
-                    // quest_view.removeChild(quest_view.lastChild);
+                let quest_view_box = document.createElement("div");
+                quest_view_box.classList.add("full-quest");
 
-                    let quest_view_box = document.createElement("div");
-                    quest_view_box.classList.add("full-quest");
-
-                    let border_view = document.createElement("div");
-                    border_view.classList.add("full-border");
+                let border_view = document.createElement("div");
+                border_view.classList.add("full-border");
 
 
-                    let title_view = document.createElement("span");
-                    title_view.textContent = obj.name;
+                let title_view = document.createElement("span");
+                title_view.textContent = obj.name;
 
-                    let description_view = document.createElement("span");
+                let description_view = document.createElement("span");
 
-                    let tag_view = document.createElement("span");
-                    tag_view.textContent = obj.tags;
-                    tag_view.classList.add("full-tags");
+                let tag_view = document.createElement("span");
+                tag_view.textContent = obj.tags;
+                tag_view.classList.add("full-tags");
 
-                    let status_view = document.createElement("span");
-                    status_view.classList.add("full-status");
-                    if (obj.isCompleted) {
-                        status_view.textContent = "Completed";
-                    } else {
-                        status_view.textContent = "Incomplete";
-                    }
-
-                    let desc_view = document.createElement("span");
-                    desc_view.textContent = obj.desc;
-                    desc_view.classList.add("full-desc");
-
-                    let img_view = document.createElement("img");
-                    img_view.id = "quest-image";
-                    img_view.src = obj.imgPth;
-
-                    png24b(obj.imgPth)
-                    .then((value) => {
-
-                        img_view.style.visibility = "visible"
-
-                        if (value['w'] > value['h']) {
-                            img_view.style.width = `75%`;
-                            img_view.style.height = "auto";
-                            
-                        }
-                    });
-
-                    border_view.appendChild(title_view);
-                    border_view.appendChild(status_view);
-                    border_view.appendChild(tag_view);
-                    border_view.appendChild(desc_view);
-                    border_view.appendChild(img_view);
-
-                    quest_view_box.appendChild(border_view);
-
-                    quest_view.appendChild(quest_view_box);
+                let status_view = document.createElement("span");
+                status_view.classList.add("full-status");
+                if (obj.isCompleted) {
+                    status_view.textContent = "Complete";
+                } else {
+                    status_view.textContent = "Incomplete";
                 }
-            });
 
-            let border = document.createElement("div");
-            border.classList.add("quest-border");
+                let desc_view = document.createElement("span");
+                desc_view.textContent = obj.desc;
+                desc_view.classList.add("full-desc");
 
+                let img_view = document.createElement("img");
+                img_view.id = "quest-image";
+                img_view.src = obj.imgPth;
 
-            let title = document.createElement("span");
-            title.textContent = obj.name;
+                png24b(obj.imgPth)
+                .then((value) => {
 
-            let tags = document.createElement("span");
-            tags.textContent = obj.tags;
-            tags.classList.add("quest-tags");
+                    img_view.style.visibility = "visible"
 
-            let status = document.createElement("span");
-            status.classList.add("quest-status");
-            if (obj.isCompleted) {
-                status.textContent = "Completed";
-            } else {
-                status.textContent = "Incomplete";
+                    if (value['w'] > value['h']) {
+                        img_view.style.width = `75%`;
+                        img_view.style.height = "auto";
+                        
+                    }
+                });
+
+                border_view.appendChild(title_view);
+                border_view.appendChild(status_view);
+                border_view.appendChild(tag_view);
+                border_view.appendChild(desc_view);
+                border_view.appendChild(img_view);
+
+                quest_view_box.appendChild(border_view);
+
+                quest_view.appendChild(quest_view_box);
             }
-
-            border.appendChild(title);
-            border.appendChild(tags);
-            border.appendChild(status);
-
-            quest.appendChild(border);
-
-            quest_list.appendChild(quest);
         });
 
-    }
+        let border = document.createElement("div");
+        border.classList.add("quest-border");
+
+
+        let title = document.createElement("span");
+        title.textContent = obj.name;
+
+        let tags = document.createElement("span");
+        tags.textContent = obj.tags;
+        tags.classList.add("quest-tags");
+
+        let status = document.createElement("span");
+        status.classList.add("quest-status");
+        if (obj.isCompleted) {
+            status.textContent = "Complete";
+        } else {
+            status.textContent = "Incomplete";
+        }
+
+        border.appendChild(title);
+        border.appendChild(tags);
+        border.appendChild(status);
+
+        quest.appendChild(border);
+
+        quest_list.appendChild(quest);
+    });
 
 }
 
