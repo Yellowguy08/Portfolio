bd2790b288c52b9ef7e1b089ca6d2ca9326b58e9
Yellowguy08 <131368912+Yellowguy08@users.noreply.github.com>
2025-06-11 16:10:23 -0400

    starting to add some timeline entries.

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
-const legend = ["mt", "valley", "plain", "forrest", "h2o"];
+//                0       1        2         3        4
+const legend = ["peak", "valley", "plain", "forrest", "h2o"];
 
-const mt_types = ["mt", "butte", "hill"];
+const peak_types = ["mt", "butte", "hill"];
 
 const h2o_types = ["lake", "sea", "river"];
 
@@ -18,10 +19,10 @@ class mapItem {
 
 let mapFeatures = [
 
-    new mapItem(),
-    new mapItem(),
-    new mapItem(),
-    new mapItem(),
+    new mapItem("First Hour of Code", legend[2], "My first experience with coding was in 1st grade doing Code.org's 'Hour of Code'", new Date("2013")),
+    new mapItem("Expedition Scratch", legend[3], "Starting a scratch account with my friend taking my coding outside of the classroom for the first time.", new Date("2019")),
+    new mapItem("Scratch Egg Takeout Released", legend[0], "My first original working game to be published to my scratch account and the main project I would focus on for a long while.", new Date("2020-12-22")),
+    new mapItem("Godot Migration", legend[1], "Due to the unity runtime fee and other leadership issues I migrated over to the godot game engine and started to remake my projects in the new engine.", new Date("2023-10-04")),
     new mapItem(),
     new mapItem(),
     new mapItem(),
